f552a36de67c78514bfcbcd46aa84d7dbbef37f1
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 01:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,18 +20,20 @@ function divide(a, b) {
 var n = 10
 
 function inc(n) {
-  return n++
+  return n ++
 }
 
 function dec(n) {
-  return n--
+  return n --
 }
 
-n = 2.222
+n = 20
 
 function makeInt(n) {
   return parseInt(n)
 }
+
+n = 2.222
 
 function preserveDecimal(n) {
   return parseFloat(n)
