cae4bf8f633347ed254faf26629b258b85fb6334
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 00:58:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-var number = 10
-
-function add5() {
-  number += 5
-}
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-console.log(number)
-
-add5()
-console.log(number)
-
-number = 10.567
+var number = 10.567
 
 function makeInt() {
   parseInt(number)
