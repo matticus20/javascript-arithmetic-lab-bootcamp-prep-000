54b82f83ddbe4475c3c54bded733b790dcef6085
Matthew Lugauskas <matthewlugauskas94@gmail.com>
2018-01-30 01:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-var number = 10.567
+var a = 5
+var b = 3 
 
-function makeInt() {
-  parseInt(number)
-  console.log(number)
+function add(a, b) {
+  return a + b 
 }
 
-function preserveDecimal() {
-  parseFloat(number)
-  console.log(number)
+function subtract(a,b) {
+  return a - b 
 }
+
+function multiply(a, b) {
+  return a * b 
+}
+
+function divide(a, b) {
+  return a / b 
+}
+
+var n = 10
+
+function inc(n) {
+  n++
+}
+inc(n)
+console.log(n)
+
+function dec(n) {
+  n--
+}
+dec(n)
+console.log(n)
+
+n = 2.222
+
+function makeInt(n) {
+  parseInt(n)
+}
+makeInt(n)
+console.log(n)
+
+function preserveDecimal(n) {
+  parseFloat(n)
+}
+preserveDecomal(n)
+console.log(n)
